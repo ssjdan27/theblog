857686aac393f08bf068e64fef4cf5ba59a45c96
Daniel Garza <danielg272313@gmail.com>
2025-07-22 21:39:58 +0000

    fix analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { Analytics } from "@vercel/analytics/next"
 
 export default function Document() {
   const meta = {
@@ -32,7 +31,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   )
